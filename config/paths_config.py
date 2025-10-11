5652fbff4064e5c76e23dcf46e7052c2bd8e9190
gokilanr <gokilanraja1428@gmail.com>
2025-10-12 00:13:48 +0530

    data preprocessing

--- config/paths_config.py
+++ config/paths_config.py
@@ -12,4 +12,8 @@ TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
 CONFIG_PATH = "config/config.yaml"
 
 
+### Data Preprocessing ###
 
+PROCESSED_DIR = "artifacts/processed"
+PROCESSED_TRAIN_PATH = os.path.join(PROCESSED_DIR, "processed_train.csv")
+PROCESSED_TEST_PATH = os.path.join(PROCESSED_DIR, "processed_test.csv")
