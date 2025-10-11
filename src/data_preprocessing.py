e0b923e577fbfd8116cf01994ea6792dfd6a6a75
gokilanr <gokilanraja1428@gmail.com>
2025-10-12 00:12:21 +0530

    cnfig yaml data preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,7 +4,14 @@ import numpy as np
 from src.logger import get_logger
 from src.custom_exception import CustomException
 from config.paths_config import *
-from utils.common_function import read_yaml,load_data
+from utils.common_function import read_yaml, load_data
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from imblearn.over_sampling import SMOTE
+
+logger = get_logger(__name__)
+
+class DataProcessor:
+
+    def __init__(self, train_path,test_path, processed_dir, config_path):
+        pass
