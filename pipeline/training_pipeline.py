49c1df2cc576618f9d7c3408d001e92ebe764172
gokilanr <gokilanraja1428@gmail.com>
2025-10-12 16:46:11 +0530

    training_pipeline

--- pipeline/training_pipeline.py
+++ pipeline/training_pipeline.py
@@ -1,3 +1,22 @@
 from src.data_ingestion import DataIngestion
 from src.data_preprocessing import DataProcessor
+from src.model_training import ModelTraining
+from src.logger import get_logger
+from config.paths_config import *
+from utils.common_function import read_yaml
 
+
+if __name__ == "__main__":
+    ## data ingestion
+
+    data_ingestion = DataIngestion(read_yaml(CONFIG_PATH))
+    data_ingestion.run()
+
+    ## data preprocessing
+
+    processor = DataProcessor(TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR, CONFIG_PATH)
+    processor.process()
+
+    ## model training
+    trainer = ModelTraining(PROCESSED_TRAIN_PATH, PROCESSED_TEST_PATH, MODEL_OUTPUT_PATH)
+    trainer.run()
